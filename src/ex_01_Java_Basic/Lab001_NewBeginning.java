c0e449022e5a02862bc0e2175cc59ddfa918cf0e
ChanduA18 <relyshekhar@gmail.com>
2025-06-09 02:00:22 +0530

    github setup

---
+++ src/ex_01_Java_Basic/Lab001_NewBeginning.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab001_NewBeginning {
+    public static void main(String[] args){
+        System.out.println("Let's start the New Journey");
+        System.out.println("New update");
+    }
+}
